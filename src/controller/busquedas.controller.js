5adbf7d172a6244c43c4f2b03452bf915e66aaf5
Ruben Torres <rubenbebe.145.rt@gmail.com>
2024-07-24 15:37:31 -0600

    feat: Agregar búsqueda de equipos, empleados y usuarios por nombre

--- src/controller/busquedas.controller.js
+++ src/controller/busquedas.controller.js
@@ -23,3 +23,67 @@ export const busquedaAsignaciones = async (req, res) => {
         return res.status(500).json({ message: 'Error en el servidor', error });
     }
 };
+
+//buscar equipos por nombre
+export const busquedaEquipos = async (req, res) => {
+    const { busqueda } = req.query;
+
+    try {
+        const [rows] = await connection.query(`
+            SELECT e.id AS equipo_id, e.nombre, e.descripcion, e.fecha_adquisision, e.estado_equipo_id
+            FROM equipo e
+            WHERE e.nombre LIKE ?
+        `, [`%${busqueda}%`]);
+
+        if (rows.length === 0) {
+            return res.status(404).json({ message: 'Equipo no encontrado' });
+        }
+
+        return res.status(200).json(rows); // Aquí enviamos los resultados de la consulta SQL
+    } catch (error) {
+        return res.status(500).json({ message: 'Error en el servidor', error });
+    }
+};
+
+//buscar empleados por nombre
+export const busquedaEmpleados = async (req, res) => {
+    const { busqueda } = req.query;
+
+    try {
+        const [rows] = await connection.query(`
+            SELECT emp.id AS empleado_id, emp.nombre, emp.email, emp.cargo
+            FROM empleados emp
+            WHERE emp.nombre LIKE ?
+        `, [`%${busqueda}%`]);
+
+        if (rows.length === 0) {
+            return res.status(404).json({ message: 'Empleado no encontrado' });
+        }
+
+        return res.status(200).json(rows); // Aquí enviamos los resultados de la consulta SQL
+    } catch (error) {
+        return res.status(500).json({ message: 'Error en el servidor', error });
+    }
+};
+
+//buscar usuarios por nombre
+export const busquedaUsuarios = async (req, res) => {
+    const { busqueda } = req.query;
+
+    try {
+        const [rows] = await connection.query(`
+            SELECT u.id AS usuario_id, u.nombre, u.apellido, u.email, u.rol, u.estado
+            FROM usuarios u
+            WHERE u.nombre LIKE ?
+        `, [`%${busqueda}%`]);
+
+        if (rows.length === 0) {
+            return res.status(404).json({ message: 'Usuario no encontrado' });
+        }
+
+        return res.status(200).json(rows); // Aquí enviamos los resultados de la consulta SQL
+    } catch (error) {
+        return res.status(500).json({ message: 'Error en el servidor', error });
+    }
+};
+
--- src/routes/busquedas.route.js
+++ src/routes/busquedas.route.js
@@ -4,5 +4,8 @@ import * as busquedasCtrl from "../controller/busquedas.controller.js";
 const router = Router();
 
 router.get('/busquedas/asignaciones', busquedasCtrl.busquedaAsignaciones);
+router.get('/busquedas/equipos', busquedasCtrl.busquedaEquipos);
+router.get('/busquedas/empleados', busquedasCtrl.busquedaEmpleados);
+
 
 export default router;
