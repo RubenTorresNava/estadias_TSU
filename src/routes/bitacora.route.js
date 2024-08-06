f7bb4ce2273bf87eedfe0b17d6902b2e5527b60d
Ruben Torres <rubenbebe.145.rt@gmail.com>
2024-08-05 21:46:12 -0600

    Actualizar rutas y controlador de bitácora para obtener, actualizar y eliminar bitácora por ID desde la URL

--- src/routes/bitacora.route.js
+++ src/routes/bitacora.route.js
@@ -5,9 +5,9 @@ import verificarToken from "../middleware/token.middleware.js";
 const router = Router();
 
 router.get('/bitacoras', verificarToken, bitacoraCtrl.obtenerBitacoras);
-router.get('/bitacoras/:id', bitacoraCtrl.obtenerBitacoraIDURL);
-router.put('/bitacoras/:id', bitacoraCtrl.actualizarBitacoraIDURL);
-router.delete('/bitacoras/:id', bitacoraCtrl.eliminarBitacoraIDURL);
-router.post('/bitacoras', bitacoraCtrl.crearBitacora);
+router.get('/bitacoras/:id', verificarToken, bitacoraCtrl.obtenerBitacoraIDURL);
+router.put('/bitacoras/:id', verificarToken, bitacoraCtrl.actualizarBitacoraIDURL);
+router.delete('/bitacoras/:id', verificarToken, bitacoraCtrl.eliminarBitacoraIDURL);
+router.post('/bitacoras', verificarToken, bitacoraCtrl.crearBitacora);
 
 export default router;
