977368100ec4f60b5ca00733c7753468073afe0f
Ruben Torres <rubenbebe.145.rt@gmail.com>
2024-08-05 21:47:16 -0600

    feat: Agregar middleware de verificación de token para las rutas de búsqueda de asignaciones, equipos y empleados

--- src/routes/busquedas.route.js
+++ src/routes/busquedas.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import * as busquedasCtrl from "../controller/busquedas.controller.js";
+import verificarToken from "../middleware/token.middleware.js";
 
 const router = Router();
 
-router.get('/busquedas/asignaciones', busquedasCtrl.busquedaAsignaciones);
-router.get('/busquedas/equipos', busquedasCtrl.busquedaEquipos);
-router.get('/busquedas/empleados', busquedasCtrl.busquedaEmpleados);
+router.get('/busquedas/asignaciones', verificarToken, busquedasCtrl.busquedaAsignaciones);
+router.get('/busquedas/equipos', verificarToken, busquedasCtrl.busquedaEquipos);
+router.get('/busquedas/empleados', verificarToken, busquedasCtrl.busquedaEmpleados);
 
 
 export default router;
