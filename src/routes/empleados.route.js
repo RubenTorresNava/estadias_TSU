66551e791e32d074bafb2ed6846d79394e273d69
Ruben Torres <rubenbebe.145.rt@gmail.com>
2024-08-05 21:52:12 -0600

    feat: Agregar middleware de verificación de token para las rutas de empleados

--- src/routes/empleados.route.js
+++ src/routes/empleados.route.js
@@ -3,10 +3,10 @@ import * as empleadosCtrl from "../controller/empleados.controller.js";
 import verificarToken from "../middleware/token.middleware.js";
 const router = Router();
 
-router.post('/empleados', empleadosCtrl.insertarEmpleado);
+router.post('/empleados', verificarToken, empleadosCtrl.insertarEmpleado);
 router.get('/empleados', verificarToken, empleadosCtrl.obtenerEmpleados);
-router.get('/empleados/:id', empleadosCtrl.obtenerEmpleadoIDURL);
-router.put('/empleados/:id', empleadosCtrl.actualizarEmpleado);
-router.delete('/empleados/:id', empleadosCtrl.eliminarEmpleado);
+router.get('/empleados/:id', verificarToken, empleadosCtrl.obtenerEmpleadoIDURL);
+router.put('/empleados/:id', verificarToken, empleadosCtrl.actualizarEmpleado);
+router.delete('/empleados/:id', verificarToken, empleadosCtrl.eliminarEmpleado);
 
 export default router;
